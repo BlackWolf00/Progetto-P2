ec6f44e00f7e9ff8f673da6bafdfede923af4882
Matteo Tossuto <matteotossuto@gmail.com>
2021-04-20 19:43:10 +0200

    Update classe Rivista

--- gerarchia/rivista.cpp
+++ gerarchia/rivista.cpp
@@ -3,10 +3,24 @@
 Rivista::Rivista(string t, string g, double p, int u, string e)
     : Item(t, g, p), numeroUscita(u), editore(e) {}
 
+bool Rivista::operator==(const Item& i) const {
+    return dynamic_cast<const Rivista*>(&i) && Item::operator==(i) && numeroUscita==(static_cast<const Rivista*>(&i))->getNumeroUscita() &&
+            editore==(static_cast<const Rivista*>(&i))->getEditore();
+}
+
+bool Rivista::operator!=(const Item& i) const {
+    return dynamic_cast<const Rivista*>(&i) && Item::operator!=(i) && numeroUscita!=(static_cast<const Rivista*>(&i))->getNumeroUscita() &&
+            editore!=(static_cast<const Rivista*>(&i))->getEditore();
+}
+
 int Rivista::getNumeroUscita() const {
     return numeroUscita;
 }
 
 string Rivista::getEditore() const {
     return editore;
+}
+
+string Rivista::print() const {
+    return Item::print() + "\n" + "Numero Uscita: " + std::to_string(numeroUscita) + " Editore " + editore + "\n";
 }
--- gerarchia/rivista.h
+++ gerarchia/rivista.h
@@ -12,9 +12,20 @@ private:
     string editore;
 public:
     Rivista(string t, string g, double p, int u, string e);
+    Rivista* clone() const = 0;
+
+    bool operator==(const Item&) const;
+    bool operator!=(const Item&) const;
 
     virtual int getNumeroUscita() const;
     virtual string getEditore() const;
+
+    virtual string getAutore() const;
+
+    QString getType() const = 0;
+    string print() const;
+    virtual void serializzaDati(QXmlStreamWriter&) const = 0;
+
 };
 
 #endif // RIVISTA_H
