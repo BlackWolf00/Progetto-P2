468dba1657d67621be930a7007277eb1db411838
Matteo Tossuto <matteotossuto@gmail.com>
2021-05-03 17:24:22 +0200

    Aggiornato il main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
-#include "mainwindow.h"
+#include <iostream>
 #include <QApplication>
+#include "controller/controller.h"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    Controller c;
 
     return a.exec();
 }
